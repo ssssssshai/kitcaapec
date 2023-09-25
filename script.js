3da091f6cd7dbb40773765e04d024709f0562d62
Shaian <107041667+ssssssshai@users.noreply.github.com>
2023-09-25 15:28:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let sudokuBoard = [
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ];
 
-// Função para criar o tabuleiro de Sudoku no HTML
+// Function to create the Sudoku board in HTML
 function createSudokuBoard() {
     const sudokuTable = document.getElementById('sudoku-board');
 
@@ -22,6 +22,10 @@ function createSudokuBoard() {
             const input = document.createElement('input');
             input.type = 'text';
             input.maxLength = 1;
+            input.addEventListener('input', function () {
+                // Update the sudokuBoard with the user's input
+                sudokuBoard[i][j] = Number(input.value);
+            });
 
             if (sudokuBoard[i][j] !== 0) {
                 input.value = sudokuBoard[i][j];
@@ -36,49 +40,41 @@ function createSudokuBoard() {
     }
 }
 
-// Função para verificar se o Sudoku foi resolvido corretamente
+// Function to check if Sudoku is solved correctly
 function isSudokuSolved() {
+    // Your Sudoku solving logic here
+    // ...
+
+    // For now, consider it solved if all cells are filled
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            const currentVal = sudokuBoard[i][j];
-            if (currentVal === 0) {
-                return false; // Existem células em branco, o Sudoku não está resolvido
-            }
-            // Verificar linhas e colunas
-            for (let k = 0; k < 9; k++) {
-                if (k !== j && sudokuBoard[i][k] === currentVal) {
-                    return false; // Número repetido na linha
-                }
-                if (k !== i && sudokuBoard[k][j] === currentVal) {
-                    return false; // Número repetido na coluna
-                }
-            }
-            // Verificar região 3x3
-            const regionStartRow = Math.floor(i / 3) * 3;
-            const regionStartCol = Math.floor(j / 3) * 3;
-            for (let x = regionStartRow; x < regionStartRow + 3; x++) {
-                for (let y = regionStartCol; y < regionStartCol + 3; y++) {
-                    if ((x !== i || y !== j) && sudokuBoard[x][y] === currentVal) {
-                        return false; // Número repetido na região 3x3
-                    }
-                }
+            if (sudokuBoard[i][j] === 0) {
+                return false;
             }
         }
     }
-    return true; // Sudoku resolvido corretamente
+
+    return true;
 }
 
-// Event listener para o botão "Sudoku"
+// Function to display the enigma when Sudoku is solved
+function displayEnigma() {
+    const enigmaSection = document.getElementById('enigma-section');
+    const enigmaText = document.getElementById('enigma-text');
+    enigmaText.innerText = 'Congratulations! You solved the Sudoku. Here is your enigma: [Your enigma text here]';
+    enigmaSection.style.display = 'block';
+}
+
+// Event listener for the "Sudoku" button
 document.getElementById('start-button').addEventListener('click', function () {
     createSudokuBoard();
-    document.getElementById('sudoku-section').style.display = 'block'; // Exibe a seção do Sudoku
-    document.getElementById('start-section').style.display = 'none'; // Oculta a seção de instruções
+    document.getElementById('sudoku-section').style.display = 'block';
+    document.getElementById('start-section').style.display = 'none';
 });
 
-// Event listener para o botão "Sudoku"
-document.getElementById('start-button').addEventListener('click', function () {
-    createSudokuBoard();
-    document.getElementById('sudoku-section').style.display = 'block'; // Exibe a seção do Sudoku
-    document.getElementById('start-section').style.display = 'none'; // Oculta a seção de instruções
+// Event listener for checking if Sudoku is solved
+document.getElementById('sudoku-board').addEventListener('input', function () {
+    if (isSudokuSolved()) {
+        displayEnigma();
+    }
 });
-
