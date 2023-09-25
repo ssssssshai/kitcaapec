64e9c06f9e9517a1497f958fe73f51bbb04c3d8d
Shaian <107041667+ssssssshai@users.noreply.github.com>
2023-09-25 16:34:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-// Função para gerar um Sudoku menor (5x5) com mais células em branco
-function generateSmallSudokuWithMoreBlanks() {
-    // Sudoku 5x5 representado como uma matriz 5x5 com mais células em branco
+// Function to generate a Sudoku puzzle (simplified)
+function generateSudoku() {
+    // Sudoku puzzle represented as a 9x9 array
     const puzzle = [
-        [4, 0, 2, 0, 0],
-        [0, 0, 0, 0, 0],
-        [3, 0, 1, 0, 0],
-        [0, 0, 0, 4, 0],
-        [2, 0, 0, 3, 0]
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
     ];
 
-    // Converta o quebra-cabeça em uma tabela 5x5
+    // Convert the puzzle to a 9x9 table
     const table = document.createElement('table');
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 9; i++) {
         const row = document.createElement('tr');
 
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < 9; j++) {
             const cell = document.createElement('td');
             if (puzzle[i][j] !== 0) {
                 cell.textContent = puzzle[i][j];
@@ -24,7 +28,7 @@ function generateSmallSudokuWithMoreBlanks() {
                 const input = document.createElement('input');
                 input.setAttribute('type', 'number');
                 input.setAttribute('min', '1');
-                input.setAttribute('max', '5');
+                input.setAttribute('max', '9');
                 input.setAttribute('size', '1');
                 cell.appendChild(input);
             }
@@ -37,22 +41,15 @@ function generateSmallSudokuWithMoreBlanks() {
     return table;
 }
 
-// Event listener para verificar a solução do Sudoku
-document.getElementById('smallSudokuWithMoreBlanksContainer').addEventListener('input', function () {
-    if (isSmallSudokuWithMoreBlanksSolved()) {
-        console.log("Sudoku resolvido corretamente!");
-    }
-});
-
-// Função para verificar se o Sudoku menor com mais células em branco está resolvido corretamente
-function isSmallSudokuWithMoreBlanksSolved() {
-    const inputs = document.querySelectorAll('#smallSudokuWithMoreBlanksContainer input[type="number"]');
+// Function to check if the Sudoku is solved correctly
+function isSudokuSolved() {
+    const inputs = document.querySelectorAll('input[type="number"]');
     const puzzle = [];
 
-    // Extrair o estado atual do quebra-cabeça
+    // Extract the current state of the puzzle
     let rowIndex = 0;
     for (let i = 0; i < inputs.length; i++) {
-        if (i % 5 === 0) {
+        if (i % 9 === 0) {
             puzzle.push([]);
             rowIndex++;
         }
@@ -60,15 +57,41 @@ function isSmallSudokuWithMoreBlanksSolved() {
         puzzle[rowIndex - 1].push(isNaN(inputValue) ? 0 : inputValue);
     }
 
-    // Verificar linhas e colunas para duplicatas
-    for (let i = 0; i < 5; i++) {
+    // Check rows, columns, and 3x3 grids for duplicates
+    for (let i = 0; i < 9; i++) {
         const row = puzzle[i];
         const column = puzzle.map(row => row[i]);
+        const grid = puzzle.slice(Math.floor(i / 3) * 3, Math.floor(i / 3) * 3 + 3)
+            .map(row => row.slice((i % 3) * 3, (i % 3) * 3 + 3))
+            .flat();
 
-        if (hasDuplicates(row) || hasDuplicates(column)) {
+        if (
+            hasDuplicates(row) ||
+            hasDuplicates(column) ||
+            hasDuplicates(grid)
+        ) {
             return false;
         }
     }
 
     return true;
 }
+
+// Function to check for duplicates in an array
+function hasDuplicates(array) {
+    const values = [];
+    for (let value of array) {
+        if (value !== 0 && values.includes(value)) {
+            return true;
+        }
+        values.push(value);
+    }
+    return false;
+}
+
+// Event listener for checking Sudoku solution
+document.getElementById('sudokuContainer').addEventListener('input', function () {
+    if (isSudokuSolved()) {
+        revealRiddle();
+    }
+});
