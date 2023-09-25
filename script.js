10b3e2576a6af84e9f62240b527158468415157e
Shaian <107041667+ssssssshai@users.noreply.github.com>
2023-09-25 17:23:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function generateSudoku() {
         [1, 4, 0, 2, 0],
         [0, 0, 1, 0, 4],
         [5, 0, 0, 0, 2],
-        [0, 0, 1, 0, 3]
+        [0, 0, 0, 0, 3]
     ];
 
     // Defina a variável global 'enigma' com um enigma específico
